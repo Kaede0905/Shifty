656828c0d5852b9aa67d03d916e4b96950c24bb7
Kaede <kaede905@i.softbank.jp>
2025-11-23 17:33:19 +0900

    Fix undefined method 'option' in LINE OAuth strategy

--- api_ShiftMaker/lib/omniauth/strategies/line.rb
+++ api_ShiftMaker/lib/omniauth/strategies/line.rb
@@ -1,8 +1,25 @@
-option :authorize_params, %i[scope]
+# option :authorize_params, %i[scope]
 
-def authorize_params
-  super.tap do |params|
-    extra = request.params.slice("mode") # POSTのhidden fieldを拾う
-    params[:state] = extra.to_json if extra.present?
+# def authorize_params
+#   super.tap do |params|
+#     extra = request.params.slice("mode") # POSTのhidden fieldを拾う
+#     params[:state] = extra.to_json if extra.present?
+#   end
+# end
+# lib/omniauth/strategies/line.rb
+module OmniAuth
+  module Strategies
+    class Line < OmniAuth::Strategies::OAuth2
+      # option はクラス内で宣言
+      option :authorize_params, %i[scope]
+
+      # authorize_params メソッドも同じクラス内で
+      def authorize_params
+        super.tap do |params|
+          extra = request.params.slice("mode") # POSTのhidden fieldを拾う
+          params[:state] = extra.to_json if extra.present?
+        end
+      end
+    end
   end
 end
