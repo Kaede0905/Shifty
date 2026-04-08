afafc11e1100ff128d301de172d4033d5dd6c631
Kaede <kaede905@i.softbank.jp>
2026-04-09 01:22:08 +0900

    edit migrate3

--- api_ShiftMaker/db/migrate/20251007143412_fix_shift_foreign_key.rb
+++ api_ShiftMaker/db/migrate/20251007143412_fix_shift_foreign_key.rb
@@ -1,6 +1,9 @@
-class FixShiftForeignKey < ActiveRecord::Migration[7.2]
+class FixShiftForeignKeys < ActiveRecord::Migration[7.2]
   def change
-    remove_foreign_key :shifts, column: :store_connect_id rescue nil
-    add_foreign_key :shifts, :employee_store_assignments, column: :store_connect_id
+    # stores外部キーがあれば削除
+    remove_foreign_key :shifts, :stores, if_exists: true
+
+    # employee_store_assignments外部キーがなければ追加
+    add_foreign_key :shifts, :employee_store_assignments, column: :store_connect_id, if_not_exists: true
   end
 end
