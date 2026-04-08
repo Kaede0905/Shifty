3f59bb751effed1ae88f49d5acc92da120593cf4
Kaede <kaede905@i.softbank.jp>
2026-04-09 02:50:19 +0900

    Fix Line strategy option scope placement

--- api_ShiftMaker/lib/omniauth/strategies/line.rb
+++ api_ShiftMaker/lib/omniauth/strategies/line.rb
@@ -7,19 +7,15 @@
 #   end
 # end
 # lib/omniauth/strategies/line.rb
-require "omniauth/strategies/oauth2"
-
+# lib/omniauth/strategies/line.rb
 module OmniAuth
   module Strategies
     class Line < OmniAuth::Strategies::OAuth2
-      # ここで option を設定
-      option :name, "line"
       option :authorize_params, %i[scope]
 
-      # authorize_params のオーバーライド
       def authorize_params
         super.tap do |params|
-          extra = request.params.slice("mode") # POSTのhidden fieldを拾う
+          extra = request.params.slice("mode")
           params[:state] = extra.to_json if extra.present?
         end
       end
