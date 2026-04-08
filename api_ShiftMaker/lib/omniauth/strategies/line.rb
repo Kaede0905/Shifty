e7f8ef071d9cdbab304d341d9c04b9a96fd6717a
Kaede <kaede905@i.softbank.jp>
2026-04-09 02:43:45 +0900

    Fix undefined method  by moving inside class

--- api_ShiftMaker/lib/omniauth/strategies/line.rb
+++ api_ShiftMaker/lib/omniauth/strategies/line.rb
@@ -1,8 +1,28 @@
-option :authorize_params, %i[scope]
+# option :authorize_params, %i[scope]
 
-def authorize_params
-  super.tap do |params|
-    extra = request.params.slice("mode") # POSTのhidden fieldを拾う
-    params[:state] = extra.to_json if extra.present?
+# def authorize_params
+#   super.tap do |params|
+#     extra = request.params.slice("mode") # POSTのhidden fieldを拾う
+#     params[:state] = extra.to_json if extra.present?
+#   end
+# end
+# lib/omniauth/strategies/line.rb
+require "omniauth/strategies/oauth2"
+
+module OmniAuth
+  module Strategies
+    class Line < OmniAuth::Strategies::OAuth2
+      # ここで option を設定
+      option :name, "line"
+      option :authorize_params, %i[scope]
+
+      # authorize_params のオーバーライド
+      def authorize_params
+        super.tap do |params|
+          extra = request.params.slice("mode") # POSTのhidden fieldを拾う
+          params[:state] = extra.to_json if extra.present?
+        end
+      end
+    end
   end
 end
